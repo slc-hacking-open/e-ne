1fb502e2230d1689e2b12b0e2a58d32dd8f0ebab
miyagon <hiim.shu.m@gmail.com>
2019-11-18 05:12:26 +0000

    update: add action.type SEND

--- src/actions/post.ts
+++ src/actions/post.ts
@@ -1,7 +1,17 @@
 export const EMPATHY = "EMPATHY";
+export const SEND = "SEND";
 
 export const empathy = () => ({
   type: EMPATHY as typeof EMPATHY,
 });
 
-export type PostAction = ReturnType<typeof empathy>;
+export const send = (contents: string, from: string, to: string) => ({
+  type: SEND as typeof SEND,
+  payload: {
+    contents,
+    from,
+    to,
+  },
+});
+
+export type PostAction = ReturnType<typeof empathy> | ReturnType<typeof send>;
