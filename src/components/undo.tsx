baa42739ed123285693e53a9b56f19c233ca6421
minami-tnk <minami0530tanaka@gmail.com>
2019-11-29 08:36:04 +0000

    clean: apply error on prettier

--- src/components/undo.tsx
+++ src/components/undo.tsx
@@ -5,16 +5,13 @@ export interface UndoProps {
   undo?: () => void;
 }
 
-const Undo: FC<UndoProps> = ({
-  undoCount = "",
-  undo = () => { }
-}) => (
-    <div>
-      <p>{undoCount}個の投稿があります！</p>
-      <button type="button" onClick={undo}>１つ戻す</button>
-    </div>
-
-  );
+const Undo: FC<UndoProps> = ({ undoCount = "", undo = () => {} }) => (
+  <div>
+    <p>{undoCount}個の投稿があります！</p>
+    <button type="button" onClick={undo}>
+      １つ戻す
+    </button>
+  </div>
+);
 
 export default Undo;
-
