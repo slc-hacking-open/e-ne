50fc3964b774d364e0cd979db18df77b0e294215
miyagon <hiim.shu.m@gmail.com>
2019-12-16 08:34:00 +0000

    fix: thunkが動かない

--- src/actions/post.ts
+++ src/actions/post.ts
@@ -1,23 +1,27 @@
+import { Dispatch, Action } from "redux";
+import { ThunkAction } from "redux-thunk";
+import { AppState } from "../reducer";
+
 export const EMPATHY = "EMPATHY";
 
-/*
-export const empathy = (id: number) => ({
+export const empathy = (response: any) => ({
   type: EMPATHY as typeof EMPATHY,
   payload: {
-    id
+    response
   }
 });
-*/
 
-export const empathy = (id: number) => {
-  console.log("empathy id:", id);
-
-  return {
-    type: EMPATHY as typeof EMPATHY,
-    payload: {
-      id
-    }
-  };
+export const pushEmpathy = (
+  id: number
+): ThunkAction<void, AppState, undefined, PostAction> => (
+  dispatch: Dispatch<Action>
+) => {
+  fetch("localhost:3030/empathy")
+    .then(res => {
+      console.log(res);
+      dispatch(empathy(res));
+    })
+    .catch(reason => {});
 };
 
 export type PostAction = ReturnType<typeof empathy>;
--- src/containers/post.tsx
+++ src/containers/post.tsx
@@ -1,17 +1,23 @@
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
+import { ThunkDispatch } from "redux-thunk";
 
-import { empathy } from "../actions/post";
+import { PostAction, empathy, pushEmpathy } from "../actions/post";
+import { AppState } from "../reducer";
 import Post from "../components/post";
 
 interface DispatchProps {
   empathy: (id: number) => void;
+  pushEmpathy: (id: number) => void;
 }
 
 const mapStateToProps = () => ({});
 
-const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
-  empathy: (id: number) => dispatch(empathy(id))
+const mapDispatchToProps = (
+  dispatch: ThunkDispatch<AppState, undefined, PostAction>
+): DispatchProps => ({
+  empathy: (id: number) => dispatch(empathy(id)),
+  pushEmpathy: (id: number) => dispatch(pushEmpathy(id))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Post);
