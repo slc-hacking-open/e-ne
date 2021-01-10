a4dda893630a631ea125353cea40e69c388c4035
YoshikoshiMasahiro <m.yoshikoshi106@gmail.com>
2021-01-10 18:27:11 +0900

    ログイン仮ページ作成

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import Amplify from 'aws-amplify'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
 import './index.css'
 import store from './store'
+
+Amplify.configure({
+  Auth: {
+    // IDプールのID
+    identityPoolId: '86a10237-d073-4d73-8872-cae12aa2c254',
+    region: 'Ius-east-2',
+    // ユーザープールのID
+    userPoolId: 'us-east-2_4jNhofe8w',
+    // アプリクライアントのID
+    userPoolWebClientId: '46cai9allh85tv1vevg9rqs9v6',
+  },
+})
 
 ReactDOM.render(
   <Provider store={store}>
